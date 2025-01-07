58c7cb275dfc4584162b099aa2b6c537ea64ce43
TanmayXpapa <zskv1000aura@gmail.com>
2025-01-07 09:43:31 +0530

    Update m.py

--- m.py
+++ m.py
@@ -307,8 +307,8 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert port to integer
             time = int(command[3])  # Convert time to integer
-            if time > 600:
-                response = "Error: Time interval must be less than 600."
+            if time > 60000:
+                response = "Error: Time interval must be less than 60000."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
